5819688d31e31e2fed00b5dd03b103e5e2cafc32
rasel <russelroy15@gmail.com>
2021-09-14 17:30:26 +0600

    WIP on slider js porject

---
+++ app.js
@@ -0,0 +1,14 @@
+const images = [
+  "images/img-1.jpg",
+  "images/img-2.jpg",
+  "images/img-3.jpg",
+  "images/img-4.jpg",
+  "images/img-5.jpg",
+  "images/img-6.jpg",
+  "images/img-7.jpg",
+  "images/img-8.jpg",
+  "images/img-9.jpg",
+  "images/img-10.jpg",
+];
+
+setInterval(() => {}, 1000);
