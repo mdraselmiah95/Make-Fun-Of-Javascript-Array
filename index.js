90b082f35d23aaa6be94f44954757faf3e780e08
mdraselmiah95 <russelroy15@gmail.com>
2023-01-07 19:16:11 +0600

    Learning Array

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const arr = [1, 2, 34, 6, 7, 8, 89];
+//* Array Literal=> []
+
+const arr = [];
+
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[99] = 9;
+console.log(arr);
+console.log(arr.length - 1);
