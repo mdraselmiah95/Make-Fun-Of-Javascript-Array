3405bcd3d221714c611b81ab3267b94628588d4a
mdraselmiah95 <russelroy15@gmail.com>
2023-01-07 20:51:52 +0600

    Learning Array Traverse

--- TraverseArray.js
+++ TraverseArray.js
@@ -14,9 +14,16 @@ const x = 1,
 //* Simple Traverse
 
 for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+  //   console.log(arr[i]);
 }
 
 //* Array Sun & Avg
+
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
 
 //* Find Large Number
