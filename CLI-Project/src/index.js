de6eb83283714b38910bae0bd692059ba097e59d
mdraselmiah95 <russelroy15@gmail.com>
2023-01-21 23:25:33 +0600

    WIP CLI Project TODOS

--- CLI-Project/src/index.js
+++ CLI-Project/src/index.js
@@ -6,9 +6,60 @@ const { ADD, UPDATE, NEXT, DONE, FIND, LIST } = require("./commands");
 
 const fileName = "../data.json";
 const filePath = path.resolve(__dirname, fileName);
+
 (function init() {
   const data = readFile(filePath) || [];
   const todo = new Todo(data);
   const { _: baseCommand } = argv;
-  console.log(argv);
+  //   console.log(argv);
+  switch (baseCommand[0]) {
+    case ADD: {
+      todo.addItem(argv.text);
+      console.log("Todo Added");
+      satisfies(todo.todoList, filePath);
+      break;
+    }
+    case UPDATE: {
+      todo.update(argv.id, argv.text);
+      console.log("TOdo Updated");
+      saveFile(todo.todoList, filePath);
+      break;
+    }
+    case NEXT: {
+      const item = todo.next();
+      console.log(`${item.id} - ${item.text} [${item.created}]`);
+      break;
+    }
+    case DONE: {
+      todo.done();
+      console.log("One Item Completed");
+      saveFile(todo.todoList, filePath);
+      break;
+    }
+    case FIND: {
+      const items = todo.find(argv.term);
+      if (items.length === 0) {
+        console.log("No Item Found ⛲");
+        break;
+      }
+      for (let i = 0; i < items.length; i++) {
+        console.log(`${items[i].id} - ${items[i].text} [${items[i].created}]`);
+      }
+      break;
+    }
+    case LIST: {
+      if (todo.todoList.length === 0) {
+        console.log("Empty list ⛲");
+        break;
+      }
+      for (let i = 0; i < todo.todoList.length; i++) {
+        console.log(
+          `${todo.todoList[i].id} - ${todo.todoList[i].text} [${todo.todoList[i].created}]`
+        );
+      }
+      break;
+    }
+    default:
+      throw new Error("Command Not Found");
+  }
 })();
