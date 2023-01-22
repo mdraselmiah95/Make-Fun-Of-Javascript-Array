c0172322072fcfd83e9573f1ab19c81bf5f2e322
mdraselmiah95 <russelroy15@gmail.com>
2023-01-23 00:06:08 +0600

    JavaScript stack

---
+++ array-as-a-stack/basic-stack.js
@@ -0,0 +1,46 @@
+/**
+ * @title : Implement A Basic Stack
+ */
+
+const MAX_SIZE = 38;
+
+class Stack {
+  constructor() {
+    this.list = new Array(MAX_SIZE);
+    this.top = -1;
+  }
+
+  push(item) {
+    if (this.top >= MAX_SIZE) {
+      console.log("Stack Overflow 💮");
+      return false;
+    }
+    this.list[++this.top] = item;
+    return true;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      console.log("stack Underflow");
+      return false;
+    }
+    let item = this.list[this.top];
+    delete this.list[this.top];
+    this.top--;
+    return item;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      console.log("Stack Underflow");
+      return false;
+    }
+    return this.list[this.top];
+  }
+
+  isEmpty() {
+    return this.top < 0;
+  }
+}
+
+//ok
