5b91b64de08529bf0ae85997fae0f45e1561244d
mdraselmiah95 <russelroy15@gmail.com>
2023-01-24 01:12:39 +0600

    Array iterate

--- iterate-array-elements/forEach.js
+++ iterate-array-elements/forEach.js
@@ -10,9 +10,44 @@ let names = [
 ];
 
 for (let i = 0; i < names.length; i++) {
-  console.log(names);
+  //   console.log(names);
 }
 
 for (let i = 0; i < names.length; i++) {
-  console.log(`${i + 1}: ${names[i]}`);
+  //   console.log(`${i + 1}: ${names[i]}`);
 }
+
+//* Iterating Logic=> Business Logic
+
+function reuse(arr, logic) {
+  for (let i = 0; i < arr.length; i++) {
+    logic(arr[i], i, arr);
+  }
+}
+
+function log(value) {
+  //   console.log(value);
+}
+
+function logic(value, index, arr) {
+  //   console.log(value, index, arr);
+}
+reuse([1, 2, 3], log);
+reuse(["Rasel", "Sakib", "Joy"], logic);
+
+function myForEach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i, arr);
+  }
+}
+
+const num = [1, 2, 3, 4, 5];
+let sum = 0;
+
+function sumFunc(v) {
+  sum += v;
+}
+
+myForEach(num, sumFunc);
+
+console.log(sum);
