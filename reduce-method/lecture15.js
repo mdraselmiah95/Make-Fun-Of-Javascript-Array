6323290d34768aeb0a846eb4f16d32b8b37ee82d
mdraselmiah95 <russelroy15@gmail.com>
2023-01-25 03:36:41 +0600

    Array reduce method

---
+++ reduce-method/lecture15.js
@@ -0,0 +1,51 @@
+/**
+ * @title :Combining Data from different Sources
+ */
+
+const courses = [
+  {
+    id: 1,
+    name: "Javascript All you need to know",
+    topic: "javascript",
+    price: 2599,
+  },
+  {
+    id: 2,
+    name: "Dive into NodeJS",
+    topic: "NodeJS",
+    price: 3500,
+  },
+  {
+    id: 3,
+    name: "Understanding React core Feature",
+    topic: "react",
+    price: 4500,
+  },
+  {
+    id: 4,
+    name: "Understanding React advance Feature",
+    topic: "react",
+    price: 5500,
+  },
+  {
+    id: 5,
+    name: "Add to Cart - MERN Project",
+    topic: "mern",
+    price: 5900,
+  },
+  {
+    id: 6,
+    name: "Make fun of javaScript Array",
+    topic: "javascript",
+    price: 1999,
+  },
+];
+
+const discountedPrice = {
+  1: 1500,
+  2: 3000,
+  3: 4000,
+  4: 5000,
+  5: 5500,
+  6: 1500,
+};
