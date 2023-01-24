0e5fec5386029fb9d2f24b76effd4d69904ecdb3
mdraselmiah95 <russelroy15@gmail.com>
2023-01-24 16:25:35 +0600

    JavaScipt Reduce Method

---
+++ reduce-method/lecture.js
@@ -0,0 +1,15 @@
+/**
+ * @title :Understand the concept of Reduce 🎊
+ */
+
+const arr = [1, 2, 3, 4, 5, 6];
+const names = ["Rasel", "Shakib", "Rafi", "Saif"];
+const votes = [
+  "Java",
+  "Java",
+  "Python",
+  "Java",
+  "JavaScript",
+  "Python",
+  "JavaScript",
+];
