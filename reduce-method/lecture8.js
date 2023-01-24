a179fe84e09fee4b888b22df9c8cba94ce9eebd5
mdraselmiah95 <russelroy15@gmail.com>
2023-01-24 21:34:00 +0600

    Array reduce method

--- reduce-method/lecture8.js
+++ reduce-method/lecture8.js
@@ -10,4 +10,22 @@ const products = [
   { name: "Head First Java", price: 400 },
   { name: "JavaScript Cookbook", price: 369 },
   { name: "JavaScript Cookbook", price: 369 },
+  { name: "Head First Java", price: 400 },
+  { name: "Head First Python", price: 200 },
+  { name: "Head First C++", price: 650 },
 ];
+
+const invoice = products.reduce((acc, cur) => {
+  if (acc[cur.name]) {
+    acc[cur.name].quantity++;
+    acc[cur.name].price += cur.price;
+  } else {
+    acc[cur.name] = {
+      price: cur.price,
+      quantity: 1,
+    };
+  }
+  return acc;
+}, {});
+
+console.log(invoice);
