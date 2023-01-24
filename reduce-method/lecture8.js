be330546dec9298b32e164e8e7e0e25cd5932d7c
mdraselmiah95 <russelroy15@gmail.com>
2023-01-24 19:55:02 +0600

    Array reduce method

---
+++ reduce-method/lecture8.js
@@ -0,0 +1,13 @@
+/**
+ * @title :Reduce Array init single Object
+ */
+
+const products = [
+  { name: "JavaScript Cookbook", price: 369 },
+  { name: "Head First Python", price: 200 },
+  { name: "Head First Java", price: 400 },
+  { name: "JavaScript Cookbook", price: 350 },
+  { name: "Head First Java", price: 400 },
+  { name: "JavaScript Cookbook", price: 369 },
+  { name: "JavaScript Cookbook", price: 369 },
+];
