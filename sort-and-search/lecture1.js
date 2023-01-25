459ef0d0eec00abaae6842608c139435b754b649
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 02:17:06 +0600

    JavaScript Array Sorting and Searching

--- sort-and-search/lecture1.js
+++ sort-and-search/lecture1.js
@@ -1,4 +1,32 @@
 /**
  * @title :Understand the concept of Sorting 🎊
- * @definition :make smaller or less in amount, degree, or size
+ * @definition :The sort() method sorts the elements of an array in place and returns the reference to the same array, now sorted.
  */
+
+// TODO: Common Sorting Algorithms in JavaScript
+/**
+ * Bubble Sort
+ * Selection Sort
+ * Insertion Sort
+ * Merge Sort
+ * Quick Sort
+ * Bucket Sort
+ */
+
+const names = [
+  "Angelina Jolie",
+  "Scarlett Johansson",
+  "Jennifer Lawrence",
+  "Emily Blunt",
+  "Elizabeth Olsen",
+  "Zendaya",
+  "Anya Taylor-Joy",
+  "Shailene Woodley",
+];
+
+/**
+ * @title :Implement Bubble Sort
+ * @@definition :Bubble sort is a sorting algorithm that compares adjacent elements and swaps them if they do not follow the desired order.
+ */
+
+let myArray = [9, 1, 4, 6, 8, 9, 30, 22, 3, 4, 53, 76, 57, 8, 23, 34, 33, 223];
