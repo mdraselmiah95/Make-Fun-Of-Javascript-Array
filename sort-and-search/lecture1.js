3a79382c33368cd893c95af6dc5ad9ae16673ce3
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 02:27:16 +0600

    JavaScript Array Sorting and Searching

--- sort-and-search/lecture1.js
+++ sort-and-search/lecture1.js
@@ -30,3 +30,18 @@ const names = [
  */
 
 let myArray = [9, 1, 4, 6, 8, 9, 30, 22, 3, 4, 53, 76, 57, 8, 23, 34, 33, 223];
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+};
+console.log("UnSorted", myArray);
+bubbleSort(myArray);
+console.log("Sorted", myArray);
