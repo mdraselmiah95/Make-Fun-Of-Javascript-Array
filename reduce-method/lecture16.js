5ca133328e176f7039059b0f8219302687692481
mdraselmiah95 <russelroy15@gmail.com>
2023-01-25 21:27:40 +0600

    JavaScript Array reduce

--- reduce-method/lecture16.js
+++ reduce-method/lecture16.js
@@ -1,3 +1,28 @@
 /**
  * @title :Promise Chain
  */
+
+function createPromise(id) {
+  const randTime = Math.floor(Math.random() * 2000 + 100);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Processing ${id} - Time ${randTime}`);
+      resolve(id);
+    }, randTime);
+  });
+}
+
+const ids = [1, 2, 3, 4, 5, 6];
+// ids.forEach((v) => {
+//   createPromise(v);
+// });
+
+const result = ids.reduce((acc, cur) => {
+  return acc.then(() => {
+    return createPromise(cur);
+  });
+}, Promise.resolve());
+
+result.then(() => {
+  console.log("Done !!!");
+});
