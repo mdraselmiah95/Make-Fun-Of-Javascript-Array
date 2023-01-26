78f441e32dd86b706cddc22aa640eb205e080b1b
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 20:23:05 +0600

    Learning Javascript Other Method

--- other-array-method/lecture1.js
+++ other-array-method/lecture1.js
@@ -23,3 +23,23 @@ const mapper = new Map([
 Array.from(mapper.values()); // ["a", "b"]
 
 Array.from(mapper.keys()); // ["1", "2"]
+
+function f() {
+  return Array.from(arguments);
+}
+
+f(1, 2, 3, 4, 5); // [1,2,3,4,5]
+
+Array.from([1, 2, 3], (x) => x * x);
+
+Array.from({ length: 5 }, (v, i) => i * 2);
+
+const range = (start, stop, step) => {
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+};
+
+range(0, 4, 1); // [0,1,2,3,4]
+
+range(1, 10, 2); // [0,1,,3,5,7,9]
+
+// 8:12
