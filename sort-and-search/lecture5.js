a4d5eb03c6d5497cc8f313d16e55dfdb50b7a5f1
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 15:29:58 +0600

    Learning Javascript searching

---
+++ sort-and-search/lecture5.js
@@ -0,0 +1,19 @@
+/**
+ * @title :Understand and Implement Linear Search 🏇
+ */
+
+const arr = [1, 3, 4, 7, 98, 30, 5, 90, 3242, 09, 432, 878, 323, 41, 08, 91];
+
+const actress = [
+  { id: 2, name: "Angelina Jolie" },
+  { id: 4, name: "Scarlett Johansson" },
+  { id: 6, name: "Jennifer Lawrence" },
+  { id: 11, name: "Anne Hathaway" },
+  { id: 5, name: "Emily Blunt" },
+  { id: 3, name: "Elizabeth Olsen" },
+  { id: 9, name: "Zendaya" },
+  { id: 1, name: "Anya Taylor-Joy" },
+  { id: 7, name: "Shailene Woodley" },
+  { id: 8, name: "Anne Hathaway" },
+  { id: 10, name: "Anne Hathaway" },
+];
