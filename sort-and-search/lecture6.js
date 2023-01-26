d697200c0f7dc094007fc0a778e0f3406d8237d9
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 17:22:57 +0600

    Learning Javascript searching

--- sort-and-search/lecture6.js
+++ sort-and-search/lecture6.js
@@ -3,17 +3,22 @@
  * @definition :Binary Search is a searching technique which works on the Divide and Conquer approach 🥈
  */
 
-let arr = [12, 35, 3, 66, 8, 9, 13, 19, 22, 7, 18, 5, 1];
+let array = [12, 35, 3, 66, 8, 9, 13, 19, 22, 7, 18, 5, 1];
 
 const binarySearch = (arr, key) => {
   let lowest = 0;
   let highest = arr.length - 1;
 
   while (lowest <= highest) {
-    let mid = Math.floor((lowest - highest) / 2);
+    let mid = Math.floor((lowest + highest) / 2);
     if (arr[mid] === key) return mid;
     if (arr[mid] < key) lowest = mid + 1;
     if (arr[mid] > key) highest = mid - 1;
   }
   return -1;
 };
+
+array.sort((a, b) => a - b);
+console.log(array);
+const myNum = binarySearch(array, 3);
+console.log(myNum);
