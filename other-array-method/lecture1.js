615601ba753a0657698ed3f4afc1eca9c2b1592e
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 20:14:11 +0600

    Learning Javascript Other Method

--- other-array-method/lecture1.js
+++ other-array-method/lecture1.js
@@ -3,5 +3,23 @@
  */
 
 const arr = [];
-arr.concat(); // Instance Method
-Array.from(); // Static Method
+// arr.concat();  Instance Method
+// Array.from();  Static Method
+
+// TODO: Example of Javascript Static Method
+// Array.from()
+const s = Array.from("Stack Learner");
+console.log(s);
+
+const set = new Set(["foo", "Bar", "foo", "Bar"]);
+const s1 = Array.from(set);
+console.log(s1);
+
+const mapper = new Map([
+  ["1", "a"],
+  ["2", "b"],
+]);
+
+Array.from(mapper.values()); // ["a", "b"]
+
+Array.from(mapper.keys()); // ["1", "2"]
