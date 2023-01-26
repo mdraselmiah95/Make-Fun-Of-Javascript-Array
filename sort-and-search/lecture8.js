c7786dbcf6adaff4a217aab68ccb0e830c29e71b
mdraselmiah95 <russelroy15@gmail.com>
2023-01-26 18:10:14 +0600

    Learning Javascript searching

---
+++ sort-and-search/lecture8.js
@@ -0,0 +1,19 @@
+/**
+ * @title :Find Method in JavaScript🏇
+ * @definition :The find() method returns the value of the first element that passes a test.
+ */
+
+const todos = [
+  { id: "123", task: "Eat", time: "Morning" },
+  { id: "234", task: "Code", time: "Afternoon" },
+  { id: "562", task: "Coffee Break", time: "Evening" },
+  { id: "232", task: "Sleep", time: "Night" },
+];
+
+const result = [
+  { name: "Rasel", score: 99 },
+  { name: "Robin", score: 91 },
+  { name: "Shakib", score: 81 },
+  { name: "Mehedi", score: 79 },
+  { name: "Shakib", score: 93 },
+];
